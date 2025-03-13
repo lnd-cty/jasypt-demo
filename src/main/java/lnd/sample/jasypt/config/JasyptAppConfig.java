40bc8323a38dfbd7d7d9e0c9f8123bd30aa5ed5e
SGVF\nguyen.dinh-long <nguyen.dinh-long@hdsaison.com.vn>
2025-03-13 14:49:02 +0700

    u

--- src/main/java/lnd/sample/jasypt/config/JasyptAppConfig.java
+++ src/main/java/lnd/sample/jasypt/config/JasyptAppConfig.java
@@ -1,11 +1,10 @@
 package lnd.sample.jasypt.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan({"lnd.sample.app02.service"})
+// @ComponentScan({"lnd.sample.app02.service"}) //Khong can dung cho @Service !?
 @PropertySource("application.properties")
 public class JasyptAppConfig {
 
