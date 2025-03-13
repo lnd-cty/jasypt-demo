2c9b7542de54fe7c28bca97640339d4002684079
SGVF\nguyen.dinh-long <nguyen.dinh-long@hdsaison.com.vn>
2025-03-13 16:08:03 +0700

    Dùng @Import với @Configuration

--- src/main/java/lnd/sample/jasypt/JasyptDemoApplication.java
+++ src/main/java/lnd/sample/jasypt/JasyptDemoApplication.java
@@ -2,10 +2,20 @@ package lnd.sample.jasypt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import lnd.sample.jasypt.config.JasyptAppConfig;
 
 @SpringBootApplication
 public class JasyptDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JasyptDemoApplication.class, args);
+        
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(JasyptAppConfig.class)) {
+            for (String beanName : applicationContext.getBeanDefinitionNames()) {
+                System.out.println("Bean name: " + beanName);
+            }
+        }
+
+//        SpringApplication.run(JasyptDemoApplication.class, args);
     }
 }
--- src/main/java/lnd/sample/jasypt/config/JasyptAppConfig.java
+++ src/main/java/lnd/sample/jasypt/config/JasyptAppConfig.java
@@ -1,11 +1,14 @@
 package lnd.sample.jasypt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-// @ComponentScan({"lnd.sample.app02.service"}) //Khong can dung cho @Service !?
+//@Import({ DatabaseConfig.class, SwaggerConfig.class }) // Add DatabaseConfig.class vao se bi vang loi
+@Import({ SwaggerConfig.class })
+// @ComponentScan({"lnd.sample.app02.service"}) //Khong can dung cho @Service, !?
+// PropertyServiceForJasyptStartervan van @Autowired thanh cong)
 @PropertySource("application.properties")
 public class JasyptAppConfig {
-
 }
